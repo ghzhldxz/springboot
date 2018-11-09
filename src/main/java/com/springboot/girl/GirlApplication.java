693575322885ed3631f50427e6a22aa76a250368
guanhuizhenwj64144 <guanhuizhenwj64144@touna.cn>
2018-11-09 15:49:02 +0800

    springboot整合 swagger2

--- src/main/java/com/springboot/girl/GirlApplication.java
+++ src/main/java/com/springboot/girl/GirlApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.lang.ref.SoftReference;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -17,6 +19,33 @@ import java.util.List;
 public class GirlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GirlApplication.class, args);
+		//int i = 1 / 0;
+		//int j = 1 % 0;
+		double k = 1.0 / 0;//double和float可以除以0,Infinity
+		System.out.println(k);
+		//Integer[] strings = new Integer[];
+
+		/*List<?>[] lists = new ArrayList<?>[2];
+		List<Integer>[] lsa = (List<Integer>[]) Array.newInstance(ArrayList.class, 4);
+		Object o = lsa;
+		Object[] oa = (Object[]) o;
+		List<String> li = new ArrayList<String>();
+		li.add("asdf");
+		oa[1] = li;
+		System.out.println("=========================="+lsa[1].get(0));*/
+		//SpringApplication.run(GirlApplication.class, args);
+
+
+		List<String>[] lsa = (List<String>[])Array.newInstance(ArrayList.class, 4);
+		Object o = lsa;
+		Object[] oa = (Object[]) o;
+		List<Integer> li = new ArrayList<Integer>();
+		li.add(new Integer(3));
+// Correct.
+		oa[1] = li;
+// Run time error, but cast is explicit.
+		System.out.println("=========================="+(lsa[1].get(0)));  //2
+
+
 	}
 }
